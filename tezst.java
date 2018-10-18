9b2f887c80520a81f074e550c0f0f0ead61a36be
GoorEM <dencharaku2@icloud.com>
2018-10-18 16:52:41 +0900

    java commit

--- tezst.java
+++ tezst.java
@@ -1,5 +1,23 @@
 class JaBean{
 
-    private boolan 
+
+    private boolean isNullValable ;
+    private boolean canRead;
+    private String  userName;
+    private String  fileName;
+
+    public JaBean(){
+      this.userName =
+      this.fileName
+    }
+
+    public JaBean(String userName,String fileName){
+      this.userName = userName;
+      this.fileName = fileName;
+
+    }
+    public void setUserName(String userName){
+      
+    }
 
 }
